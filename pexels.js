44cc31e7aa2f6c5c7d49dc00678dd895272dba1e
Niccolò Albanese <nick_albanese@virgilio.it>
2024-11-14 16:19:17 +0100

    load done

--- pexels.js
+++ pexels.js
@@ -1,53 +1,59 @@
 const link = 'https://api.pexels.com/v1/search?query=[your-query]'
-const link2 ='https://api.pexels.com/v1/search?query=[your-secondary-query]'
+const link2 = 'https://api.pexels.com/v1/search?query=[your-secondary-query]'
 
 const load = document.getElementById('loadBtn')
 const load2 = document.getElementById('loadBtn2')
 
 load.addEventListener('click', function() {
-fetch (link, {
-    method: 'GET',
-    headers:{
-        'Content-Type':'application/json',
-        'Authorization':'GXr4WbmzLhPHglXzbTG5wW7oz4aarWFTia3W1HoR82B9zKe68QVIDxmm'
-    }
-})
-.then((response) =>{
-    if (response.ok){
-        return response.json()
-    } else {
-        throw new Error("Errore server");        
-    }})
-    .then ((images) => {
-        console.log('immagini',images)
-
+    fetch(link, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'GXr4WbmzLhPHglXzbTG5wW7oz4aarWFTia3W1HoR82B9zKe68QVIDxmm'
+        }
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            throw new Error("Errore server")        
+        }
+    })
+    .then((data) => {
+        const imgArr = document.querySelectorAll('img')
+        console.log('Immagini:', data)
+        for (let i = 0; i < imgArr.length; i++) {
+            imgArr[i].setAttribute('src', data.photos[i].src.medium)
+        }   
     })
     .catch((error) => {
         console.log('Errore', error)
     })
-    
 })
 
 load2.addEventListener('click', function() {
-fetch (link2, {
-    method: 'GET',
-    headers:{
-        'Content-Type':'application/json',
-        'Authorization':'GXr4WbmzLhPHglXzbTG5wW7oz4aarWFTia3W1HoR82B9zKe68QVIDxmm'
-    }
-})
-.then((response) =>{
-    if (response.ok){
-        return response.json()
-    } else {
-        throw new Error("Errore server");        
-    }})
-    .then ((images) => {
-        console.log('immagini',images)
-
+    fetch(link2, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'GXr4WbmzLhPHglXzbTG5wW7oz4aarWFTia3W1HoR82B9zKe68QVIDxmm'
+        }
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Errore server");        
+        }
+    })
+    .then((data) => {
+        const imgArr = document.querySelectorAll('img')
+        console.log('Immagini:', data)
+        for (let i = 0; i < imgArr.length; i++) {
+            imgArr[i].setAttribute('src', data.photos[i].src.medium)
+        }   
     })
     .catch((error) => {
         console.log('Errore', error)
     })
-    
 })
